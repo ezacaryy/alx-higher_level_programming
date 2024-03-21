df4288f28ff351a1bb95365291dd4c75bf0c4aed
Zakaria Cheikh <133505628+ezacaryy@users.noreply.github.com>
2024-03-20 18:25:16 -0700

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if not matrix:
+        return None
+    num_rows = len(matrix)
+    num_cols = len(matrix[0])
+    new_matrix = [[0] * num_cols for _ in range(num_rows)]
+    for i in range(num_rows):
+        for j in range(num_cols):
+            new_matrix[i][j] = matrix[i][j] ** 2
+    return new_matrix
